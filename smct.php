f53dca2f05400533cfdfd6bcde49c1eb486a8c8a
XerrorKonoha - Sistem <liyabaanuata4@gmail.com>
2024-10-16 08:53:15 +0700

    Add files via upload

---
+++ smct.php
@@ -0,0 +1,37 @@
+<html><title>Hacked by Asep-IT</title><link href="https://xcruzz.blogspot.com/favicon.ico" rel='shortcut icon' />
+<meta content='Hacked by Asep-IT' name='description'/>
+<meta content='Hacked by Asep-IT' name='keywords'/>
+<meta content='Hacked by Exsploit Wmc' name='Abstract'/></style> 
+<body bgcolor="#282828" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
+<center>
+<embed src="https://www.youtube.com/v/sW8ljVoORiQ&autoplay=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>.
+<br scrollamount="5" scrolldelay="50" width="100%"><link href="https://fonts.googleapis.com/css?family=Rancho" rel='stylesheet' type='text/css'>
+<body> 
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<center>
+<br><br><img src="https://d.top4top.io/p_31897qv5x0.png" width="330px" height="330px"><br/>
+<font face="Rancho" size="6" color="white"><br>Hacked by Asep-IT</font>
+</center>
+<br scrollamount="5" scrolldelay="50" width="100%"><link href="https://fonts.googleapis.com/css?family=Rancho" rel='stylesheet' type='text/css'>
+<body>
+<br scrollamount="5" scrolldelay="50" width="100%"><link href="https://fonts.googleapis.com/css?family=Tangerine" rel='stylesheet' type='text/css'>
+<body> 
+<center>
+<font face="Tangerine" size="7" color="white">UnitedCyberTeam - SabunMandi Cyber Team</font>
+<br>
+<font face="Rancho" size="2" color="white"> Jangan biarkan mesin mengendalikan hidupmu <br>kamu yang berkuasa atas keputusanmu.!<br><br>./Fathan Is Noobs - Tn Jones - Mario Ganz - Galang10 - RootDream403 - Fajar Ganz - Asep-IT - Mr Fn4ticHZ - Regi Gans -Tahu Petis - Yusron<br><font color="red">[ PLAY MUSIC ]</font>
+<HTML>
+<script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'8c2f350dcb7d9e9c',t:'MTcyNjMwNDY0Mi4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script><div><body align="center" oncontextmenu="return false" onclick="play()">  <body align="center" oncontextmenu="return false" onclick="play()"><div class="flex-center position-ref full-height">
+	<div class="content">
+<div class="text">
+	<audio id="sec" __idm_id__="823202817"> 		<source src="https://f.top4top.io/m_31837ftr10.mp3" type="audio/mpeg"></audio> 
+</font>
+</center>
+</div>
+<script>
+ var x = document.getElementById("sec");
+function play() {
+x.play(); 	
+}
+      </script>
